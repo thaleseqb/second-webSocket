2b0729671bfd929abb430c74feed3ede0754d807
thales <thalesquadros2@gmail.com>
2024-05-12 16:51:10 -0300

    construct the user validation

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -2,12 +2,14 @@
 import registrarEventosCadastro from "./registrarEventos/cadastro.js";
 import registrarEventoDocument from "./registrarEventos/documento.js";
 import registrarEventosInicio from "./registrarEventos/inicio.js";
+import registrareventosLogin from "./registrarEventos/login.js";
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
 
-  registrarEventosInicio(socket, io)
-  registrarEventoDocument(socket, io)
-  registrarEventosCadastro(socket, io)
+  registrarEventosCadastro(socket, io);
+  registrareventosLogin(socket, io);
+  registrarEventosInicio(socket, io);
+  registrarEventoDocument(socket, io);
 
 });
