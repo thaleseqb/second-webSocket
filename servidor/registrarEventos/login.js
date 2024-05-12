3cf9bedb3eebacdb382550f7ab0765c51c9da33a
thales <thalesquadros2@gmail.com>
2024-05-12 17:18:46 -0300

    user validation comparing wrote password and hash password from db

--- servidor/registrarEventos/login.js
+++ servidor/registrarEventos/login.js
@@ -1,10 +1,23 @@
 import { encontrarUsuario } from "../db/usuariosDb.js";
+import autenticarUsuario from "../utils/autenticarUsuario.js";
 
 function registrareventosLogin(socket, io) {
     socket.on("autenticar_usuario", async ({nome, senha}) => {
         const usuario = await encontrarUsuario(nome);
 
-        console.log(usuario);
+        if (usuario) {
+            const autenticado = autenticarUsuario(senha, usuario);
+    
+            if (autenticado) {
+                socket.emit("autenticacao_sucesso");
+            } else {
+                socket.emit("autenticacao_erro");
+            }
+
+        } else {
+            socket.emit("usuario_nao_encontrado")
+        }
+
     })
 }
 
