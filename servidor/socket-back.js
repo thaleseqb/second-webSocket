d28a2cc70889dda7d91c6a5cb8adba2e4cfb80fc
thales <thalesquadros2@gmail.com>
2024-05-16 15:59:04 -0300

    block user acess that doesnt have the jwt

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -5,12 +5,22 @@ import registrarEventoDocument from "./registrarEventos/documento.js";
 import registrarEventosInicio from "./registrarEventos/inicio.js";
 import registrareventosLogin from "./registrarEventos/login.js";
 import io from "./servidor.js";
+import autorizarUsuario from "./middlewares/autorizarUsusario.js";
 
-io.on("connection", (socket) => {
+const nspUsuarios = io.of("/usuarios");
+
+nspUsuarios.use(autorizarUsuario);
+
+nspUsuarios.on("connection", (socket) => {
+  
+  registrarEventosInicio(socket, nspUsuarios);
+  registrarEventoDocument(socket, nspUsuarios);
+
+})
+
+io.of("/").on("connection", (socket) => {
 
   registrarEventosCadastro(socket, io);
   registrareventosLogin(socket, io);
-  registrarEventosInicio(socket, io);
-  registrarEventoDocument(socket, io);
 
 });
